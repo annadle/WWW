81b473ccf0c7b18966e0251c7d32f70cefd420d6
annadle <adhle1230@gmail.com>
2025-08-29 18:18:41 +1000

    removed default app features

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,24 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const onclick = async () => {
-    let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id! },
-      func: () => {
-        document.body.style.backgroundColor = "red";
-      },
-    });
-  };
+  const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => onclick}>Click me</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+    <div>
+      <h1>Title</h1>
       <div className="dropdown">
-        <button className="dropbtn">Dropdown</button>
-        <div className="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-        </div>
+        <button className="dropbtn" onClick={() => setOpen(!open)}>
+          Choose your font
+        </button>
+        {open && (
+          <div className="dropdown-content">
+            <a href="#">Open Dyslexic</a>
+            <a href="#">Arial</a>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
