700cf792c28b7a8cd77ad4b67b3c87263a90d703
annadle <adhle1230@gmail.com>
2025-08-29 15:30:22 +1000

    fixed popup code in case we need it

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 async function sayHello() {
-    let tab = await chrome.tabs.query({active: true}); // query active tabs and pull out the currently active tab
+    let [tab] = await chrome.tabs.query({active: true}); // query active tabs and pull out the currently active tab
     chrome.scripting.executeScript({
-        target: { tabID: tab.id }, //pass ID of taget tab we got from active tab
+        target: { tabId: tab.id }, //pass ID of taget tab we got from active tab
         func: () => {
             alert('Hello from my extension')
         }
-    })
+    });
 }
 document.getElementById("myButton").addEventListener("click", sayHello)
