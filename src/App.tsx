7ac5a32816c693e7e9a124b0416ec4c16e5a113a
annadle <adhle1230@gmail.com>
2025-08-30 22:08:53 +1000

    adjustments to README.md and manifest.json name

--- src/App.tsx
+++ src/App.tsx
@@ -3,70 +3,67 @@ import "./App.css";
 import useFont from "./useFont";
 
 function App() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    useFont();
+  useFont();
 
-    const fontChange = (fontName: string) => {
-        setOpen(false);
+  const fontChange = (fontName: string) => {
+    setOpen(false);
 
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            const tab = tabs[0];
-            if (!tab || !tab.id) {
-                return;
-            }
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+      if (!tab || !tab.id) {
+        return;
+      }
 
-            chrome.scripting.executeScript(
-                {
-                    target: { tabId: tab.id },
-                    files: ["content.js"], // Must exist in /dist
-                },
-                () => {
-                    chrome.tabs.sendMessage(tab.id!, {
-                        action: "changeFont",
-                        font: fontName,
-                    });
-                }
-            );
-        });
-    };
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tab.id },
+          files: ["content.js"], // Must exist in /dist
+        },
+        () => {
+          chrome.tabs.sendMessage(tab.id!, {
+            action: "changeFont",
+            font: fontName,
+          });
+        }
+      );
+    });
+  };
 
-    return (
-        <div id="root">
-            <h1>Welcome to FontShift</h1>
+  return (
+    <div id="root">
+      <h1>Welcome to FontShift</h1>
 
-            <div className="dropdown">
-                <button className="button-21" onClick={() => setOpen(!open)}>
-                    Choose your font
-                </button>
+      <div className="dropdown">
+        <button className="button-21" onClick={() => setOpen(!open)}>
+          Choose your font
+        </button>
 
-                {open && (
-                    <div className="dropdown-content">
-                        <button
-                            className="button-21"
-                            onClick={() => fontChange("open-dyslexic")}
-                            style={{ fontFamily: "OpenDyslexic, sans-serif" }}
-                        >
-                            Open Dyslexic
-                        </button>
-                        <button
-                            className="button-21"
-                            onClick={() => fontChange("arial")}
-                            style={{ fontFamily: "Arial, sans-serif" }}
-                        >
-                            Arial
-                        </button>
-                        <button
-                            className="button-21"
-                            onClick={() => fontChange("default")}
-                        >
-                            Default
-                        </button>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+        {open && (
+          <div className="dropdown-content">
+            <button
+              className="button-21"
+              onClick={() => fontChange("open-dyslexic")}
+              style={{ fontFamily: "OpenDyslexic, sans-serif" }}
+            >
+              Open Dyslexic
+            </button>
+            <button
+              className="button-21"
+              onClick={() => fontChange("arial")}
+              style={{ fontFamily: "Arial, sans-serif" }}
+            >
+              Arial
+            </button>
+            <button className="button-21" onClick={() => fontChange("default")}>
+              Default
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
